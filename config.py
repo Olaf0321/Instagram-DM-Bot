9eaf9869f57fa10cbf7619e2b3cab512c3d7c764
Olaf <daikifujimoto344@gmail.com>
2025-03-18 01:06:43 +0900

    feature config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# Predefined correct username and password for login
+ORIGINAL_USERNAME = "correctusername123"
+ORIGINAL_PASSWORD = "correctpassword123"
+
+# Styling Variables
+LABEL_FONT = ("Arial", 10, "bold")
+ENTRY_FONT = ("Arial", 10)
+BTN_FONT = ("Arial", 10, "bold")
+ENTRY_BG = "#40444B"  # Dark entry background
+ENTRY_FG = "#FFFFFF"  # White text
+BTN_BG = "#7289DA"  # Discord-style blue
+BTN_FG = "#FFFFFF"  # White button text
+HOVER_BG = "#5B6EAE"  # Lighter blue on hover
+
+# DMs/Hours Options
+DMS_HOURS_OPTIONS = ["choose option", "50DMs/24Hours", "25DMs/12Hours", "10DMs/8Hours"]
+
+EMAIL_OPTIONS = ["choose email", "scagencyn@gmail.com", "gigiscage@gmail.com", "hundsonsfpd@outlook.com", "rogerzamora1936@mailtrust.online", "mariossimeonidis255@gmail.com"]
+
+# Login URL
+LOGIN_URL = "https://www.instagram.com"
+# LOGIN_URL = "http://localhost"
+
+# File path
+FILE_PATH = "user_path.csv"
